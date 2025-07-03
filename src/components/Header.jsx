8c59fac75998a67e95024186583bab6a5f5e9ed8
sumin <smkl1004@snu.ac.kr>
2025-07-03 22:08:54 +0900

    Feat: create storyline_detailpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Logo = ({ className = "" }) => {
 const Logout = ({ className = "" }) => {
   return (
     <div className="flex items-center gap-[6px]">
-      <img src="/icon.svg" alt="User Icon" className="w-[44px] h-[46px]" />
+      <img src="/icon.svg" alt="Users Icon" className="w-[44px] h-[46px]" />
       <img src="/logout.svg" alt="Logout Icon" className="w-[123px] h-[46px]" />
     </div>
   );
