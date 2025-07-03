fda2c992fd6bd13650545c230a21f95418c7e53e
paul2126 <paul.mhryu@gmail.com>
2025-07-03 19:59:50 +0900

    Feat: add models to admin page

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
+from ai.models import AiSuggestion, AiAnalysis
+from activities.models import Activity
+from applications.models import Application, QuestionList
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(AiSuggestion)
+admin.site.register(AiAnalysis)
+admin.site.register(Activity)
+admin.site.register(Application)
+admin.site.register(QuestionList)
