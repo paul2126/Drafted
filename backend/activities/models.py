e6a0692b7885e681a0fcd3b376f66a6d79822228
paul2126 <paul.mhryu@gmail.com>
2025-07-03 21:32:52 +0900

    Feat: add activity, event table and rename activity embedding table

--- backend/activities/models.py
+++ backend/activities/models.py
@@ -1,16 +1,45 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField  # postgress specific
+from pgvector.django import VectorField
+
+# from django.contrib.postgres.fields import ArrayField  # postgress specific
 from users.models import Profile
 
 
-class Activity(models.Model):
+class ActivityEmbedding(models.Model):
     user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
     metadata = models.JSONField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
-    embedding = ArrayField(models.FloatField(), null=True, blank=True)
+    embedding = VectorField(dimensions=3072)
     favorite = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        db_table = "activity_embedding"
+
+
+class Activity(models.Model):
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    favorite = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
         db_table = "activity"
+
+
+class Event(models.Model):
+    activity_id = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    role = models.TextField(null=True, blank=True)
+    ability = models.TextField(null=True, blank=True)
+    background = models.TextField(null=True, blank=True)
+    my_action = models.TextField(null=True, blank=True)
+    result = models.TextField(null=True, blank=True)
+    reflection = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = "event"
