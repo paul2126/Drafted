3c0dbccb6bbb116d78382fbac979de8d7026efef
paul2126 <paul.mhryu@gmail.com>
2025-07-04 04:53:22 +0900

    Refactor: make db passowrd look at .env

--- backend/drafted/settings.py
+++ backend/drafted/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres.mrvlbmofbrjgvnwdlwjk",
-        "PASSWORD": "drafted0703",
+        "PASSWORD": env("DB_PASSWORD"),
         "HOST": "aws-0-ap-northeast-2.pooler.supabase.com",
         "PORT": "5432",
         "POOL_MODE": "session",
