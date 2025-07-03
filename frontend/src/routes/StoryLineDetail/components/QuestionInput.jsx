e79e15f508b20be9f13234bc42614bfef6ddd037
paul2126 <paul.mhryu@gmail.com>
2025-07-04 07:30:57 +0900

    Feat: fix logic bug on asking and getting answer to client

--- frontend/src/routes/StoryLineDetail/components/QuestionInput.jsx
+++ frontend/src/routes/StoryLineDetail/components/QuestionInput.jsx
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import { useModal } from "../context/ModalContext";
 import axios from "axios";
 
+import AbilitySection from "./AbilitySection";
+import ActivitySection from "./ActivitySection";
+
 const QuestionInput = ({ className = "", applicationId, questionId }) => {
   const { toggle } = useModal();
   const [questionText, setQuestionText] = useState(
     "협업 과정에서 자신의 장점이 드러났던 경험과 단점을 극복한 경험을 하나씩 소개해주세요."
   );
+  const { response, setResponse } = useModal();
   const handleSubmit = async () => {
     try {
-      await axios.post("http://localhost:8000/api/submit_question/", {
-        application_id: applicationId,
-        question_id: questionId,
-        question: questionText,
-      });
-
+      const response = await axios.post(
+        "http://54.196.221.162:8000/api/ai/analyze/",
+        {
+          application_id: applicationId,
+          question_id: questionId,
+          question: questionText,
+        }
+      );
+      console.log(response);
+      setResponse(response);
       toggle(); // 모달 열기 or 닫기
     } catch (error) {
       console.error("질문 전송 실패:", error);
