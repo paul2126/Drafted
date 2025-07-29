0eb513274dd429338eea6b5b22ce60508220273c
paul2126 <paul.mhryu@gmail.com>
2025-07-29 15:09:33 +0900

    Feat: Set default permssion class

--- drafted/settings.py
+++ drafted/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "ai",
     "drf_yasg",
     "rest_framework",
+    "utils",
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "utils.permissions.IsSupabaseAuthenticated",
+    ],
+}
