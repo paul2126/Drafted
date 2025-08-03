823f48c525c5f267ca63711c62a07f0b8b51a112
Imggaggu <ggaggu@snu.ac.kr>
2025-08-04 03:33:23 +0900

    Feat: application post& get api views

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.shortcuts import get_object_or_404
+from django.utils.timezone import now
+
+from .models import Application, QuestionList
+from .serializers import ApplicationCreateSerializer, QuestionSerializer, EventRecommendSerializer
 
 # Create your views here.
+class ApplicationCreateView(APIView):
